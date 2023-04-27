ddfbc7f7554581843e6f58dda9c955a81417c0a6
Ysharif000 <ysharif000@citymail.cuny.edu>
2023-04-27 17:11:05 -0400

    Hello World

---
+++ Assets/hello_world.cs
@@ -0,0 +1,180 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class hello_world : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+
+
+        // Hey. THis is where I delcare my variables
+        // string myName;
+        // int myMoney;
+        // int socre;
+
+
+        // This is where I initalize my variables 
+        // myName = "Y";
+        // myMoney = 100;
+        // socre = 0;
+
+
+
+        // Debug.Log("Hello World!");
+        // Debug.Log("this is the second thing I print out!");
+
+        // Debug.Log(myMoney);
+
+        // Debug.Log(myName);
+
+        // Debug.Log(socre);
+
+        // socre = 10;
+
+        // Debug.Log(socre);
+
+        // Delcar the Variables
+        // string Secondname;
+        // int Holdingbank;
+        // int amount;
+
+        // input of the variables 
+        // Secondname = "ben";
+        // Holdingbank = 1000;
+        // amount = 800;
+
+        // output of the varibles
+        // Debug.Log("My name is " + Secondname);
+        // Debug.Log("The Holding Bank are " + Holdingbank);
+        // Debug.Log("The amount I want is " + amount);
+
+        //_______________________________________________________________________
+
+        // new start 02-27-2023 If/Logic statement
+
+        //string character_Name;
+        //int points;
+        //bool Playerfound; 
+
+
+        // input of the Variables
+
+        //character_Name = "Nexus";
+        //points = 1000;
+        //Playerfound = true;
+
+
+        //if (character_Name == "Nexus")
+        //{
+        //Debug.Log("Welcome " + character_Name);
+        //}
+        //else
+        //{
+        //Debug.Log("Logout of Nexus");
+        //}
+
+        //if(Playerfound == true)
+        //{
+        //Debug.Log("You start here " + character_Name);
+        //}
+        //else if(points > 1000)
+        //{
+        //Debug.Log("You have more points");
+        //}
+
+        //else if(points < 1000)
+        //{
+        //Debug.Log("You have less point");
+        //}
+
+        //if(points == 1000)
+        //{
+        //Debug.Log("You have won " + character_Name);
+        //}
+
+        //_____________________________________
+
+        //Math & Loops 03/01/2023
+
+        //declair variables
+
+        //int a;
+        //int b;
+        //int c;
+        //int answer;
+
+        //assign variables
+
+        //a = 1;
+        //b = 2;
+        //c = 3;
+
+        //Debug.Log("Today we are going to do some math!");
+
+        //answer = 1 + 2 + 2;
+        //Debug.Log(answer);
+
+        //answer = 6 * 3;
+        //Debug.Log(answer);
+
+        //answer = a + 1;
+        //Debug.Log(answer);
+
+        // In class Exerices
+
+        //int z;
+        //int t;
+        //int q;
+        //int answer;
+        //string Main_charter_firstname;
+        //string Main_charater_lastname;
+        //string Main_charater_fullname;
+        //Debug.Log("A new start of vaule");
+        //z = 214;
+        //t = 300;
+        //q = 600;
+        //Main_charter_firstname = "Nexus";
+        //Main_charater_lastname = "Cat";
+        //Main_charater_fullname = Main_charter_firstname + Main_charater_lastname;
+        //answer = (t + q);
+       //Debug.Log(answer);
+        //answer = z*(q+q);
+        //Debug.Log(answer);
+
+        //Debug.Log("My name is " + Main_charater_fullname);
+
+        //int x;
+
+        //x = 1;
+
+        //Debug.Log("x: " + x);
+        //x = x + 2;
+        //Debug.Log("x: " + x);
+
+        // simple add 1 to current value 
+
+        //x = x + 3;
+        //x++;
+        // "++" is a special keyword, that basically translates
+        // to line right above 
+        // which means, current value + 1
+
+        //03/06/2023 
+        //Welcome to my game "Cats have nine Lives
+
+        
+        
+            
+
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
