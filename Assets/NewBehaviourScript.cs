baaf4ff51da8d87c72e4d5a5967e7523a962cd3e
Ysharif000 <ysharif000@citymail.cuny.edu>
2023-04-27 17:37:43 -0400

    03-06-2023

---
+++ Assets/NewBehaviourScript.cs
@@ -0,0 +1,73 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        // 03-06-2023
+        // Welcome to my game "Cats Have Nine Lives!" 
+        // It's not a very fun game . :(
+
+        //Working with some loop today 
+        //int numLives = 9;
+        //bool catStillAlive = true;
+
+        //while(catStillAlive == true)
+        // {
+        // Debug.Log("I'm still alive baby!");
+        // Debug.Log("lose a life!");
+        // numLives = numLives - 1; 
+        // What is another way to subtract one from itself?
+        // if(numLives == 0)
+        //{
+        //catStillAlive = false;
+        // }
+        //}
+
+        // Debug.Log("GAME OVER!");
+
+        // Example:   
+        // int playerslives = 3;
+        // bool player = true;
+
+        // while(player == true)
+        // {
+        // Debug.Log("player lives: " + playerslives);
+        // Debug.Log("I am feeling alive");
+        // Debug.Log("I lose a playerslives");
+        // playerslives = playerslives -1;
+        // if(playerslives == 0)
+        // {
+        // player = false;
+        // }
+        // }
+
+        // Debug.Log("GAME OVER!");
+
+
+        // Loops in Class Exercise 
+
+        //Initializer
+        // while (condition)
+        //  {
+        // code block;
+        // iterator;
+        //  }
+
+        
+
+
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
